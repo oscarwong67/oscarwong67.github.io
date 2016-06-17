fb8cf29a27b4f4c9f41eb1abee5a57e021be6230
Oscar Wong <oscar@wadomp.com>
2016-06-17 17:34:58 -0600

    Added second page of projects.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,25 @@
 var colors = ['#4da6ff', '#ffa31a', '#33cc33', '#ff80aa'];
 
-var random_color = colors[Math.floor(Math.random() * colors.length)];
+for (var i = 1; i <= 4; i++) {
+  var random_color = colors[Math.floor(Math.random() * colors.length)];
+  $('#project-img-' + i).css({ 'border-color': random_color });
+}
 
-$('#project-img-4').css('border-color', random_color);
-random_color = colors[Math.floor(Math.random() * colors.length)];
-$('#project-img-3').css('border-color', random_color);
-random_color = colors[Math.floor(Math.random() * colors.length)];
-$('#project-img-2').css('border-color', random_color);
-random_color = colors[Math.floor(Math.random() * colors.length)];
-$('#project-img-1').css('border-color', random_color);
+$('#projects-row-2').hide();
+$('#projects-row-1').show();
+
+$('#page-toggle').html('<i class="fa fa-arrow-circle-o-right arrow-right" aria-hidden="true"></i>');
+
+$('#page-toggle').click(function () {
+  if ($(this).html() === '<i class="fa fa-arrow-circle-o-right arrow-right" aria-hidden="true"></i>')
+    $('#page-toggle').html('<i class="fa fa-arrow-circle-o-left arrow-left" aria-hidden="true"></i>');
+  else
+    $('#page-toggle').html('<i class="fa fa-arrow-circle-o-right arrow-right" aria-hidden="true"></i>');
+  
+  $('#projects-row-1, #projects-row-2').slideToggle();
+
+  for (i = 5; i <= 8; i++) {
+    var random_color = colors[Math.floor(Math.random() * colors.length)];
+    $('#project-img-' + i).css({ 'border-color': random_color });
+  }
+});
