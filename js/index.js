84d795e1c3d25cfb04caaed5d0947d7e1f97a19f
Oscar Wong <oscar@wadomp.com>
2016-10-21 20:58:34 -0600

    Added third project page and tic tac toe project

--- js/index.js
+++ js/index.js
@@ -1,25 +1,63 @@
-var colors = ['#4da6ff', '#ffa31a', '#33cc33', '#ff80aa'];
-
-for (var i = 1; i <= 4; i++) {
-  var random_color = colors[Math.floor(Math.random() * colors.length)];
-  $('#project-img-' + i).css({ 'border-color': random_color });
-}
-
+$('#projects-row-3').hide();
 $('#projects-row-2').hide();
 $('#projects-row-1').show();
 
-$('#page-toggle').html('<i class="fa fa-arrow-circle-o-right arrow-right" aria-hidden="true"></i>');
+var strList;
+var strTwo;
+var colors = ['#4da6ff', '#ffa31a', '#33cc33', '#ff80aa'];
 
-$('#page-toggle').click(function () {
-  if ($(this).html() === '<i class="fa fa-arrow-circle-o-right arrow-right" aria-hidden="true"></i>')
-    $('#page-toggle').html('<i class="fa fa-arrow-circle-o-left arrow-left" aria-hidden="true"></i>');
-  else
-    $('#page-toggle').html('<i class="fa fa-arrow-circle-o-right arrow-right" aria-hidden="true"></i>');
-  
-  $('#projects-row-1, #projects-row-2').slideToggle();
-
-  for (i = 5; i <= 8; i++) {
+function colours() {
+  if ($('#projects-row-2').is(':visible')) {
+    i = 5;
+    x = 8;
+  }
+  else if ($('#projects-row-3').is(':visible')) {
+    i = 9;
+    x = 12;
+  }
+  else {
+    i = 1;
+    x = 4;
+  }
+  for (; i <= x; i++) {
     var random_color = colors[Math.floor(Math.random() * colors.length)];
     $('#project-img-' + i).css({ 'border-color': random_color });
   }
+}
+
+function showButtons() {
+  if ($('#projects-row-1').is(':visible')) {
+    $('#page-toggle-left').hide();
+  }
+  else if ($('#projects-row-2').is(':visible')) {
+    $('#page-toggle-left').show();
+    $('#page-toggle-right').show();    
+  }
+  else {
+    $('#page-toggle-right').hide();
+  }
+}
+showButtons();
+
+$('.page-toggle').click(function () {
+  for (var q = 1; q <= 3; q++) {
+    var str = '#projects-row-' + q;    
+    if ($(str).is(':visible')) {
+      break;
+    }
+  }
+  if ($(this).attr('id') === 'page-toggle-right') {
+    q++;
+    strTwo = '#projects-row-' + q;
+    strList = str + ", " + strTwo;
+  }
+  else {
+    q--;
+    strTwo = '#projects-row-' + q;
+    strList = str + ", " + strTwo;    
+  }
+  $(strList).slideToggle(function () {
+    showButtons();
+  });
+  
 });
