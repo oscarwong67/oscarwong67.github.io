0b4b066fcebd745ba184e182cf18445dd0d4efad
Oscar Wong <wong.oscar67@gmail.com>
2018-06-12 23:01:31 -0600

    added a footer currently not in use

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/header'
+import Footer from '../components/footer';
 import './index.css'
 
 const Layout = ({ children, data }) => (
@@ -12,7 +12,8 @@ const Layout = ({ children, data }) => (
     height: '100vh',
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    alignItems: 'center'
   }}>
     <Helmet
       title={data.site.siteMetadata.title}
@@ -21,17 +22,15 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'Oscar Wong, developer, engineer, software engineer' },
       ]}
     />
-    {/*<Header siteTitle={data.site.siteMetadata.title} />*/}
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
         padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        paddingBottom: 40,
       }}
     >
       {children()}
     </div>
+    {/* <Footer /> */}
   </div>
 )
 
